08b7e33386b2ec3819f1463fa7a9669cabcc599b
Agathon006 <agaton615@gmail.com>
2023-12-31 09:54:13 +0300

    Changed all that was suggested in pull request

--- scripts/index.js
+++ scripts/index.js
@@ -1,168 +1,161 @@
-const mainSkills = document.getElementById("main-skills");
-const mainPortfolio = document.getElementById("main-portfolio");
-const mainVideo = document.getElementById("main-video");
-const mainPrice = document.getElementById("main-price");
-const mainFeedback = document.getElementById("main-feedback");
-const headerItemsRightPartList = document.getElementById("header-items-right-part-list");
-headerItemsRightPartList.addEventListener('click', (event) => {
-  if (event.target.closest('.header__items-right-part-list-element-link-skills')) {
-    mainSkills.scrollIntoView({ behavior: 'smooth' });
-  }
-  else if (event.target.closest('.header__items-right-part-list-element-link-portfolio')) {
-    mainPortfolio.scrollIntoView({ behavior: 'smooth' });
-  }
-  else if (event.target.closest('.header__items-right-part-list-element-link-video')) {
-    mainVideo.scrollIntoView({ behavior: 'smooth' });
-  }
-  else if (event.target.closest('.header__items-right-part-list-element-link-price')) {
-    mainPrice.scrollIntoView({ behavior: 'smooth' });
-  }
-  else if (event.target.closest('.header__items-right-part-list-element-link-contacts')) {
-    mainFeedback.scrollIntoView({ behavior: 'smooth' });
-  };
+const headerItemsRightPartLanguageLinkRu = document.querySelector('#header-items-right-part-language-link-ru');
+headerItemsRightPartLanguageLinkRu.addEventListener('click', () =>
+  alert('Unfortunately, this functionality is under develepoment')
+);
 
-});
-
-
-const headerItemsRightPartMenuButton = document.getElementById("header-items-right-part-menu-button");
-const asideButton = document.getElementById("aside-button");
-const asideWrapper = document.getElementById("aside-wrapper");
+const headerItemsRightPartMenuButton = document.querySelector('#header-items-right-part-menu-button');
+const asideWrapper = document.querySelector('#aside-wrapper');
 headerItemsRightPartMenuButton.addEventListener('click', () => {
-  asideWrapper.style.visibility = 'visible';
-  asideWrapper.style.opacity = 1;
+  asideWrapper.classList.remove('aside-wrapper');
+  asideWrapper.classList.add('aside-wrapper-opened');
 });
 document.addEventListener('click', (event) => {
-  if (event.target.classList.contains('aside-wrapper') || event.target.closest('.aside__button')) {
-    asideWrapper.style.visibility = 'hidden';
-    asideWrapper.style.opacity = 0;
+  if (event.target.classList.contains('aside-wrapper-opened') || event.target.closest('#aside-button')) {
+    asideWrapper.classList.remove('aside-wrapper-opened');
+    asideWrapper.classList.add('aside-wrapper');
   }
 });
 asideWrapper.addEventListener('click', (event) => {
-  if (event.target.closest('.aside__list-element-button-skills')) {
-    mainSkills.scrollIntoView({ behavior: 'smooth' });
-    asideWrapper.style.visibility = 'hidden';
-    asideWrapper.style.opacity = 0;
+  if (event.target.closest('#header-items-right-part-list-element-link-skills')) {
+    asideWrapper.classList.remove('aside-wrapper-opened');
+    asideWrapper.classList.add('aside-wrapper');
   }
-  else if (event.target.closest('.aside__list-element-button-portfolio')) {
-    mainPortfolio.scrollIntoView({ behavior: 'smooth' });
-    asideWrapper.style.visibility = 'hidden';
-    asideWrapper.style.opacity = 0;
+  else if (event.target.closest('#header-items-right-part-list-element-link-portfolio')) {
+    asideWrapper.classList.remove('aside-wrapper-opened');
+    asideWrapper.classList.add('aside-wrapper');
   }
-  else if (event.target.closest('.aside__list-element-button-video')) {
-    mainVideo.scrollIntoView({ behavior: 'smooth' });
-    asideWrapper.style.visibility = 'hidden';
-    asideWrapper.style.opacity = 0;
+  else if (event.target.closest('#header-items-right-part-list-element-link-video')) {
+    asideWrapper.classList.remove('aside-wrapper-opened');
+    asideWrapper.classList.add('aside-wrapper');
   }
-  else if (event.target.closest('.aside__list-element-button-price')) {
-    mainPrice.scrollIntoView({ behavior: 'smooth' });
-    asideWrapper.style.visibility = 'hidden';
-    asideWrapper.style.opacity = 0;
+  else if (event.target.closest('#header-items-right-part-list-element-link-price')) {
+    asideWrapper.classList.remove('aside-wrapper-opened');
+    asideWrapper.classList.add('aside-wrapper');
   }
-  else if (event.target.closest('.aside__list-element-button-contacts')) {
-    mainFeedback.scrollIntoView({ behavior: 'smooth' });
-    asideWrapper.style.visibility = 'hidden';
-    asideWrapper.style.opacity = 0;
+  else if (event.target.closest('#header-items-right-part-list-element-link-contacts')) {
+    asideWrapper.classList.remove('aside-wrapper-opened');
+    asideWrapper.classList.add('aside-wrapper');
   };
 });
 
 
-const mainPortfolioNavigation = document.getElementById("main-portfolio-navigation");
-const mainPortfolioNavigationButtonWinter = document.getElementById("main-portfolio-navigation-button-winter");
-const mainPortfolioNavigationButtonSpring = document.getElementById("main-portfolio-navigation-button-spring");
-const mainPortfolioNavigationButtonSummer = document.getElementById("main-portfolio-navigation-button-summer");
-const mainPortfolioNavigationButtonAutumn = document.getElementById("main-portfolio-navigation-button-autumn");
-const mainPortfolioImagesImage1 = document.getElementById("main-portfolio-images-image-1");
-const mainPortfolioImagesImage2 = document.getElementById("main-portfolio-images-image-2");
-const mainPortfolioImagesImage3 = document.getElementById("main-portfolio-images-image-3");
-const mainPortfolioImagesImage4 = document.getElementById("main-portfolio-images-image-4");
-const mainPortfolioImagesImage5 = document.getElementById("main-portfolio-images-image-5");
-const mainPortfolioImagesImage6 = document.getElementById("main-portfolio-images-image-6");
+const mainPortfolioNavigation = document.querySelector('#main-portfolio-navigation');
+const mainPortfolioNavigationButtonWinter = document.querySelector('#main-portfolio-navigation-button-winter');
+const mainPortfolioNavigationButtonSpring = document.querySelector('#main-portfolio-navigation-button-spring');
+const mainPortfolioNavigationButtonSummer = document.querySelector('#main-portfolio-navigation-button-summer');
+const mainPortfolioNavigationButtonAutumn = document.querySelector('#main-portfolio-navigation-button-autumn');
+const mainPortfolioImagesImage1 = document.querySelector('#main-portfolio-images-image-1');
+const mainPortfolioImagesImage2 = document.querySelector('#main-portfolio-images-image-2');
+const mainPortfolioImagesImage3 = document.querySelector('#main-portfolio-images-image-3');
+const mainPortfolioImagesImage4 = document.querySelector('#main-portfolio-images-image-4');
+const mainPortfolioImagesImage5 = document.querySelector('#main-portfolio-images-image-5');
+const mainPortfolioImagesImage6 = document.querySelector('#main-portfolio-images-image-6');
 mainPortfolioNavigation.addEventListener('click', (event) => {
-  if (event.target.closest('.main__portfolio-navigation-button-winter')) {
-    mainPortfolioNavigationButtonWinter.style.color = 'black';
-    mainPortfolioNavigationButtonWinter.style.backgroundColor = '#bdae82';
-    mainPortfolioNavigationButtonSpring.style.color = '#bdae82';
-    mainPortfolioNavigationButtonSpring.style.backgroundColor = 'black';
-    mainPortfolioNavigationButtonSummer.style.color = '#bdae82';
-    mainPortfolioNavigationButtonSummer.style.backgroundColor = 'black';
-    mainPortfolioNavigationButtonAutumn.style.color = '#bdae82';
-    mainPortfolioNavigationButtonAutumn.style.backgroundColor = 'black';
-    mainPortfolioImagesImage1.src = '/images/winter-1.jpg';
-    mainPortfolioImagesImage2.src = '/images/winter-2.jpeg';
-    mainPortfolioImagesImage3.src = '/images/winter-3.jpg';
-    mainPortfolioImagesImage4.src = '/images/winter-4.jpeg';
-    mainPortfolioImagesImage5.src = '/images/winter-5.jpeg';
-    mainPortfolioImagesImage6.src = '/images/winter-6.jpeg';
+  if (event.target.closest('#main-portfolio-navigation-button-winter')) {
+    mainPortfolioNavigationButtonWinter.classList.add('main__portfolio-navigation-active-button');
+    mainPortfolioNavigationButtonWinter.classList.remove('main__portfolio-navigation-inactive-button');
+    mainPortfolioNavigationButtonSpring.classList.remove('main__portfolio-navigation-active-button');
+    mainPortfolioNavigationButtonSpring.classList.add('main__portfolio-navigation-inactive-button');
+    mainPortfolioNavigationButtonSummer.classList.remove('main__portfolio-navigation-active-button');
+    mainPortfolioNavigationButtonSummer.classList.add('main__portfolio-navigation-inactive-button');
+    mainPortfolioNavigationButtonAutumn.classList.remove('main__portfolio-navigation-active-button');
+    mainPortfolioNavigationButtonAutumn.classList.add('main__portfolio-navigation-inactive-button');
+    mainPortfolioImagesImage1.src = '../images/weather/winter/winter-1.jpg';
+    mainPortfolioImagesImage1.alt = 'winter-image';
+    mainPortfolioImagesImage2.src = '../images/weather/winter/winter-2.jpeg';
+    mainPortfolioImagesImage2.alt = 'winter-image';
+    mainPortfolioImagesImage3.src = '../images/weather/winter/winter-3.jpg';
+    mainPortfolioImagesImage3.alt = 'winter-image';
+    mainPortfolioImagesImage4.src = '../images/weather/winter/winter-4.jpeg';
+    mainPortfolioImagesImage4.alt = 'winter-image';
+    mainPortfolioImagesImage5.src = '../images/weather/winter/winter-5.jpeg';
+    mainPortfolioImagesImage5.alt = 'winter-image';
+    mainPortfolioImagesImage6.src = '../images/weather/winter/winter-6.jpeg';
+    mainPortfolioImagesImage6.alt = 'winter-image';
   }
-  else if (event.target.closest('.main__portfolio-navigation-button-spring')) {
-    mainPortfolioNavigationButtonWinter.style.color = '#bdae82';
-    mainPortfolioNavigationButtonWinter.style.backgroundColor = 'black';
-    mainPortfolioNavigationButtonSpring.style.color = 'black';
-    mainPortfolioNavigationButtonSpring.style.backgroundColor = '#bdae82';
-    mainPortfolioNavigationButtonSummer.style.color = '#bdae82';
-    mainPortfolioNavigationButtonSummer.style.backgroundColor = 'black';
-    mainPortfolioNavigationButtonAutumn.style.color = '#bdae82';
-    mainPortfolioNavigationButtonAutumn.style.backgroundColor = 'black';
-    mainPortfolioImagesImage1.src = '/images/spring-1.png';
-    mainPortfolioImagesImage2.src = '/images/spring-2.png';
-    mainPortfolioImagesImage3.src = '/images/spring-3.png';
-    mainPortfolioImagesImage4.src = '/images/spring-4.png';
-    mainPortfolioImagesImage5.src = '/images/spring-5.png';
-    mainPortfolioImagesImage6.src = '/images/spring-6.png';
+  else if (event.target.closest('#main-portfolio-navigation-button-spring')) {
+    mainPortfolioNavigationButtonWinter.classList.remove('main__portfolio-navigation-active-button');
+    mainPortfolioNavigationButtonWinter.classList.add('main__portfolio-navigation-inactive-button');
+    mainPortfolioNavigationButtonSpring.classList.add('main__portfolio-navigation-active-button');
+    mainPortfolioNavigationButtonSpring.classList.remove('main__portfolio-navigation-inactive-button');
+    mainPortfolioNavigationButtonSummer.classList.remove('main__portfolio-navigation-active-button');
+    mainPortfolioNavigationButtonSummer.classList.add('main__portfolio-navigation-inactive-button');
+    mainPortfolioNavigationButtonAutumn.classList.remove('main__portfolio-navigation-active-button');
+    mainPortfolioNavigationButtonAutumn.classList.add('main__portfolio-navigation-inactive-button');
+    mainPortfolioImagesImage1.src = '../images/weather/spring/spring-1.png';
+    mainPortfolioImagesImage1.alt = 'spring-image';
+    mainPortfolioImagesImage2.src = '../images/weather/spring/spring-2.png';
+    mainPortfolioImagesImage2.alt = 'spring-image';
+    mainPortfolioImagesImage3.src = '../images/weather/spring/spring-3.png';
+    mainPortfolioImagesImage3.alt = 'spring-image';
+    mainPortfolioImagesImage4.src = '../images/weather/spring/spring-4.png';
+    mainPortfolioImagesImage4.alt = 'spring-image';
+    mainPortfolioImagesImage5.src = '../images/weather/spring/spring-5.png';
+    mainPortfolioImagesImage5.alt = 'spring-image';
+    mainPortfolioImagesImage6.src = '../images/weather/spring/spring-6.png';
+    mainPortfolioImagesImage6.alt = 'spring-image';
   }
-  else if (event.target.closest('.main__portfolio-navigation-button-summer')) {
-    mainPortfolioNavigationButtonWinter.style.color = '#bdae82';
-    mainPortfolioNavigationButtonWinter.style.backgroundColor = 'black';
-    mainPortfolioNavigationButtonSpring.style.color = '#bdae82';
-    mainPortfolioNavigationButtonSpring.style.backgroundColor = 'black';
-    mainPortfolioNavigationButtonSummer.style.color = 'black';
-    mainPortfolioNavigationButtonSummer.style.backgroundColor = '#bdae82';
-    mainPortfolioNavigationButtonAutumn.style.color = '#bdae82';
-    mainPortfolioNavigationButtonAutumn.style.backgroundColor = 'black';
-    mainPortfolioImagesImage1.src = '/images/summer-1.png';
-    mainPortfolioImagesImage2.src = '/images/summer-2.png';
-    mainPortfolioImagesImage3.src = '/images/summer-3.png';
-    mainPortfolioImagesImage4.src = '/images/summer-4.png';
-    mainPortfolioImagesImage5.src = '/images/summer-5.png';
-    mainPortfolioImagesImage6.src = '/images/summer-6.png';
+  else if (event.target.closest('#main-portfolio-navigation-button-summer')) {
+    mainPortfolioNavigationButtonWinter.classList.remove('main__portfolio-navigation-active-button');
+    mainPortfolioNavigationButtonWinter.classList.add('main__portfolio-navigation-inactive-button');
+    mainPortfolioNavigationButtonSpring.classList.remove('main__portfolio-navigation-active-button');
+    mainPortfolioNavigationButtonSpring.classList.add('main__portfolio-navigation-inactive-button');
+    mainPortfolioNavigationButtonSummer.classList.add('main__portfolio-navigation-active-button');
+    mainPortfolioNavigationButtonSummer.classList.remove('main__portfolio-navigation-inactive-button');
+    mainPortfolioNavigationButtonAutumn.classList.remove('main__portfolio-navigation-active-button');
+    mainPortfolioNavigationButtonAutumn.classList.add('main__portfolio-navigation-inactive-button');
+    mainPortfolioImagesImage1.src = '../images/weather/summer/summer-1.png';
+    mainPortfolioImagesImage1.alt = 'summer-image';
+    mainPortfolioImagesImage2.src = '../images/weather/summer/summer-2.png';
+    mainPortfolioImagesImage2.alt = 'summer-image';
+    mainPortfolioImagesImage3.src = '../images/weather/summer/summer-3.png';
+    mainPortfolioImagesImage3.alt = 'summer-image';
+    mainPortfolioImagesImage4.src = '../images/weather/summer/summer-4.png';
+    mainPortfolioImagesImage4.alt = 'summer-image';
+    mainPortfolioImagesImage5.src = '../images/weather/summer/summer-5.png';
+    mainPortfolioImagesImage5.alt = 'summer-image';
+    mainPortfolioImagesImage6.src = '../images/weather/summer/summer-6.png';
+    mainPortfolioImagesImage6.alt = 'summer-image';
   }
-  else if (event.target.closest('.main__portfolio-navigation-button-autumn')) {
-    mainPortfolioNavigationButtonWinter.style.color = '#bdae82';
-    mainPortfolioNavigationButtonWinter.style.backgroundColor = 'black';
-    mainPortfolioNavigationButtonSpring.style.color = '#bdae82';
-    mainPortfolioNavigationButtonSpring.style.backgroundColor = 'black';
-    mainPortfolioNavigationButtonSummer.style.color = '#bdae82';
-    mainPortfolioNavigationButtonSummer.style.backgroundColor = 'black';
-    mainPortfolioNavigationButtonAutumn.style.color = 'black';
-    mainPortfolioNavigationButtonAutumn.style.backgroundColor = '#bdae82';
-    mainPortfolioImagesImage1.src = '/images/autumn-1.svg';
-    mainPortfolioImagesImage2.src = '/images/autumn-2.svg';
-    mainPortfolioImagesImage3.src = '/images/autumn-3.svg';
-    mainPortfolioImagesImage4.src = '/images/autumn-4.svg';
-    mainPortfolioImagesImage5.src = '/images/autumn-5.svg';
-    mainPortfolioImagesImage6.src = '/images/autumn-6.svg';
+  else if (event.target.closest('#main-portfolio-navigation-button-autumn')) {
+    mainPortfolioNavigationButtonWinter.classList.remove('main__portfolio-navigation-active-button');
+    mainPortfolioNavigationButtonWinter.classList.add('main__portfolio-navigation-inactive-button');
+    mainPortfolioNavigationButtonSpring.classList.remove('main__portfolio-navigation-active-button');
+    mainPortfolioNavigationButtonSpring.classList.add('main__portfolio-navigation-inactive-button');
+    mainPortfolioNavigationButtonSummer.classList.remove('main__portfolio-navigation-active-button');
+    mainPortfolioNavigationButtonSummer.classList.add('main__portfolio-navigation-inactive-button');
+    mainPortfolioNavigationButtonAutumn.classList.add('main__portfolio-navigation-active-button');
+    mainPortfolioNavigationButtonAutumn.classList.remove('main__portfolio-navigation-inactive-button');
+    mainPortfolioImagesImage1.src = '../images/weather/autumn/autumn-1.svg';
+    mainPortfolioImagesImage1.alt = 'autumn-image';
+    mainPortfolioImagesImage2.src = '../images/weather/autumn/autumn-2.svg';
+    mainPortfolioImagesImage2.alt = 'autumn-image';
+    mainPortfolioImagesImage3.src = '../images/weather/autumn/autumn-3.svg';
+    mainPortfolioImagesImage3.alt = 'autumn-image';
+    mainPortfolioImagesImage4.src = '../images/weather/autumn/autumn-4.svg';
+    mainPortfolioImagesImage4.alt = 'autumn-image';
+    mainPortfolioImagesImage5.src = '../images/weather/autumn/autumn-5.svg';
+    mainPortfolioImagesImage5.alt = 'autumn-image';
+    mainPortfolioImagesImage6.src = '../images/weather/autumn/autumn-6.svg';
+    mainPortfolioImagesImage6.alt = 'autumn-image';
   };
 });
 
 
-const mainVideoContainerButton = document.getElementById("main__video_container_button");
-const mainVideoContainerPlayer = document.getElementById("main__video_container_player");
-mainVideoContainerButton.addEventListener("click", () => {
-  if (mainVideoContainerPlayer.paused) {
-    mainVideoContainerPlayer.play();
-    mainVideoContainerButton.style.visibility = 'hidden';
-    mainVideoContainerPlayer.controls = 'controls';
-  } else {
-    mainVideoContainerPlayer.pause();
-  };
+const mainVideoContainerButton = document.querySelector('#main__video_container_button');
+const mainVideoContainerPlayer = document.querySelector('#main__video_container_player');
+mainVideoContainerButton.addEventListener('click', () => {
+  mainVideoContainerPlayer.play();
+  mainVideoContainerButton.style.visibility = 'hidden';
+  mainVideoContainerPlayer.controls = 'controls';
 });
 
 
-const mainFeedbackForm = document.getElementById("main__feedback_form");
-const mainFeedbackFormInputEmail = document.getElementById("main__feedback_form_input_email");
-const mainFeedbackFormInputPhone = document.getElementById("main__feedback_form_input_phone");
-const mainFeedbackFormInputMessage = document.getElementById("main__feedback_form_input_message");
-const mainFeedbackFormSendButton = document.getElementById("main-feedback-form-send-button");
+const mainFeedbackForm = document.querySelector('#main__feedback_form');
+const mainFeedbackFormInputEmail = document.querySelector('#main__feedback_form_input_email');
+const mainFeedbackFormInputPhone = document.querySelector('#main__feedback_form_input_phone');
+const mainFeedbackFormInputMessage = document.querySelector('#main__feedback_form_input_message');
+const mainFeedbackFormSendButton = document.querySelector('#main-feedback-form-send-button');
 mainFeedbackForm.addEventListener('click', (event) => {
   if (event.target.id === 'main__feedback_form_input_email') {
     mainFeedbackFormInputEmail.style.opacity = 1;
